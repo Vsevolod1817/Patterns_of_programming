9ff9680325e96afea9dfe3a91899dc8e5fbebc93
Vsevolod1817 <96308315+Vsevolod1817@users.noreply.github.com>
2023-04-29 12:53:49 +0300

    Create Student.rb

---
+++ Лаб2/2.1.2/Student.rb
@@ -0,0 +1,81 @@
+class Student
+
+  def initialize(surname, name, father_name, id: nil, phone: nil, telegram: nil, email: nil, git: nil)
+    @id = id
+    @surname=surname
+    @name=name
+    @father_name=father_name
+    @phone = phone
+    @telegram = telegram
+    @email = email
+    @git = git
+  end
+
+  # геттеры
+
+  def getid
+    @id
+  end
+
+  def getsurname
+    @surname
+  end
+
+  def getname
+    @name
+  end
+
+  def getfather_name
+    @father_name
+  end
+
+  def getphone
+    @phone
+  end
+
+  def gettelegram
+    @telegram
+  end
+
+  def getemail
+    @email
+  end
+
+  def getgit
+    @git
+  end
+
+  # сеттеры
+
+  def id=(id)
+    @id = id
+  end
+
+  def name=(name)
+    @name=name
+  end
+
+  def surname=(surname)
+    @surname=surname
+  end
+
+  def phone=(phone)
+    @phone=phone
+  end
+
+  def telegram=(telegram)
+    @telegram=telegram
+  end
+
+  def email=(email)
+    @email=email
+  end
+
+  def git=(git)
+    @git=git
+  end
+
+  def to_s
+    "id: #{@id}\nФамилия: #{@surname}\nИмя: #{@name}\nОтчество: #{@father_name}\nТелефон: #{@phone}\nТелеграм: #{@telegram}\nПочта: #{@email}\nГит: #{@git}"
+  end
+end
