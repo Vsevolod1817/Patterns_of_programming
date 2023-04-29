e22c8c48fc293023acbc6fbb295a30050860bdf9
Vsevolod1817 <96308315+Vsevolod1817@users.noreply.github.com>
2023-04-29 12:50:32 +0300

    Create main.rb

---
+++ Лаб2/2.1.2/main.rb
@@ -0,0 +1,12 @@
+require_relative 'LAB2.rb'
+
+student1 = Student.new(surname = 'Епифанцев',
+                       name = 'Всеволод',
+                       father_name = 'Александрович',
+                       id:'1',
+                       phone:'+79064796878',
+                       telegram:'@vzevolodka',
+                       email:'seva.epifantsev@gmail.com',
+                       git:'https://github.com/Vsevolod1817')
+
+puts student1.to_s
