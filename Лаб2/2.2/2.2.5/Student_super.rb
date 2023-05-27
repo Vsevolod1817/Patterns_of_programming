3f28e21538f74f539cd2e3b6b6e829998b880270
Vsevolod1817 <96308315+Vsevolod1817@users.noreply.github.com>
2023-05-27 12:09:49 +0300

    Update Student_super.rb

--- Лаб2/2.2/2.2.5/Student_super.rb
+++ Лаб2/2.2/2.2.5/Student_super.rb
@@ -1,1 +1,55 @@
+class Student_super
 
+  attr_accessor :id, :phone, :telegram, :email, :git
+
+  def fullname
+    "#{surname} #{name[0]}. #{father_name[0]}."
+  end
+
+  # validation
+  def self.right_number?(phone)
+    phone.nil? || phone.match?(/^((\+7|7|8)+([0-9]){10})$/)
+  end
+
+  def self.right_name?(name)
+    name.nil? || name.match?(/\A[a-zA-Zа-яА-Я]+\z/)
+  end
+
+  def self.right_telegram?(telegram)
+    telegram.nil? || telegram.match?(/\A@[A-Za-z0-9_]{5,32}\z/)
+  end
+
+  def self.right_email?(email)
+    email.nil? || email.match?(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
+  end
+
+  def self.right_git?(git)
+    git.nil? || git.match?(/\Ahttps:\/\/github\.com\/[a-zA-Z0-9]+\z/)
+  end
+
+  # сеттеры
+  def phone=(phone)
+    raise ArgumentError, "Неправильный формат номера" unless self.class.right_number?(phone)
+    @phone = phone
+  end
+
+  def name=(name)
+    raise ArgumentError, "Неправильный формат имени" unless self.class.right_name?(name)
+    @name=name
+  end
+
+  def telegram=(telegram)
+    raise ArgumentError, "Неправильный формат телеграма" unless self.class.right_telegram?(telegram)
+    @telegram=telegram
+  end
+
+  def email=(email)
+    raise ArgumentError, "Неправильный формат почты" unless self.class.right_email?(email)
+    @email=email
+  end
+
+  def git=(git)
+    raise ArgumentError, "Неправильный формат гита" unless self.class.right_git?(git)
+    @git=git
+  end
+end
