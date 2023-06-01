437d1a382efc60b3925a988f22fc5a81f08e7c04
Vsevolod1817 <96308315+Vsevolod1817@users.noreply.github.com>
2023-06-01 09:16:51 +0300

    Update Data_list.rb

--- Лаб2/2.3/Data_list.rb
+++ Лаб2/2.3/Data_list.rb
@@ -1,34 +1,40 @@
 class Data_list
+  # private_class_method :new
+  # public_class_method :new
+  attr_accessor :data_list, :selected_data
 
-  private_class_method :new
-  attr_reader :list
-
-  def initialize(list)
-    self.list = list
-    self.select_items = []
+  def initialize(data)
+    self.data_list = data
+    self.selected_data = []
+  end
+  def select(*numbers)
+    selected_data.append(*numbers)
+  end
+  def get_select
+    selected_data.inject([]) {|res, index| res<<data_list[index].id}
+  end
+  def clear_selected
+    self.selected_data = []
   end
 
-  #Выделить элемент по номеру
-  def select(number)
-    select_items.append(number)
-  end
-
-  #получаем массив выделеных элементов
-  def get_selected
-    return [] if select_items.empty?
-    selected_id_list = []
-    select_items.each do |num|
-      selected_id_list.append(list[num].id)
-    end
-    selected_id_list
-  end
-
-  #Этот метод будтет реализован в наследнике
+  # применение паттерна Шаблон
   def get_names
+    raise NotImplementedError, "This method is implemented in subclasses"
   end
 
-  #Этот метод будтет реализован в наследнике
-  def get_data
+  def get_information(obj)
+    raise NotImplementedError, "This method is implemented in subclasses"
+
   end
+
+  def get_data
+    student_data = []
+    self.data_list.map.with_index do |student, i|
+      student_data.append([i].append(self.get_information(student)))
+    end
+    Data_table.new(student_data)
+
+  end
+
 
 end
